de3cd5957b287e6849b4f63a2f2a354eab203390
Dhanushkumar <93417785+Idk507@users.noreply.github.com>
2025-10-22 21:35:49 +0530

    Create run_examples.sh

---
+++ run_examples.sh
@@ -0,0 +1,7 @@
+
+
+if __name__ == '__main__':
+print('Run the modules with:')
+print(' python src/react_agent.py')
+print(' python src/sequential_chain_pydantic.py')
+print(' python src/langchain_agent.py')
